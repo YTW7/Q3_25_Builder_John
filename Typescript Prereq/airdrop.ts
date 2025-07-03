ce70b3dfbced3a7190d22fb1de24091c1dc25ba4
YTW7 <myahya96@gmail.com>
2025-07-03 20:08:22 +0530

    update: airdrop script

--- Typescript Prereq/airdrop.ts
+++ Typescript Prereq/airdrop.ts
@@ -1,6 +1,5 @@
-import { Connection, Keypair, LAMPORTS_PER_SOL } from
-"@solana/web3.js"
-import wallet from "./dev-wallet.json"
+import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import wallet from "./turbin3-wallet.json"
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
